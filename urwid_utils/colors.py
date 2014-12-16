da046da37929b80484013b679ce9f52ef2426cd8
Mike Burr <miburr@cisco.com>
2014-12-16 12:15:40 +0900

    add "STYLES" to color constants (bold, etc.)

--- urwid_utils/colors.py
+++ urwid_utils/colors.py
@@ -5,6 +5,10 @@ import urwid
 from urwid.display_common import _BASIC_COLORS as BASIC_COLORS
 from urwid_utils.util import get_const_identifiers
 
-color_const = get_const_identifiers(urwid.display_common)
+STYLES = ['bold', 'underline', 'blink', 'standout']
+STYLES = zip([n.upper() for n in STYLES], STYLES)
+
+color_const = get_const_identifiers(urwid.display_common, STYLES)
+color_const['STYLES'] = STYLES
 globals().update(color_const)
 __all__ = color_const.keys()
