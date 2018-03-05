ec7e6ec916b1a34986aebc2308c5ab963b7ed991
Tony Cebzanov <tonycpsu@gmail.com>
2018-03-05 13:21:14 -0500

    Bump version: 0.1.0-dev → 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(file):
 
 setup(
     name=NAME,
-    version="0.1.0-dev",
+    version="0.1.1-dev",
     description='A collection of simple, straightforward, but extensible utilities for the urwid package.',
     # long_description=read('README.rst'),
     author='Tony Cebzanov',
--- urwid_utils/__init__.py
+++ urwid_utils/__init__.py
@@ -22,7 +22,7 @@ urwid_utils.util
      Mostly internal-used utilities.
 """
 
-__version__ = "0.1.0-dev"
+__version__ = "0.1.1-dev"
 
 from .palette import *
 from .colors import *
