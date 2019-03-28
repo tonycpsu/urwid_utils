918d8eccb416c829d3be31eb82b453b1050c682b
Tony Cebzanov <tonycpsu@gmail.com>
2019-03-28 03:03:04 -0400

    palette: Add ability to build PaletteEntry objects from configruation

--- urwid_utils/colors.py
+++ urwid_utils/colors.py
@@ -2,11 +2,29 @@
 
 import re
 import urwid
+import math
 from urwid.display_common import _BASIC_COLORS as BASIC_COLORS
+from urwid.display_common import _BASIC_COLOR_VALUES as BASIC_COLOR_VALUES
 from urwid.display_common import _ATTRIBUTES as STYLES
 from urwid_utils.util import get_const_identifiers
 
 STYLES = list(zip([n.upper() for n in STYLES], STYLES))
+
+BASIC_COLOR_MAP = dict(zip(BASIC_COLOR_VALUES, BASIC_COLORS))
+
+def rgb_distance(c1, c2):
+    (r1,g1,b1) = c1
+    (r2,g2,b2) = c2
+    return math.sqrt((r1 - r2)**2 + (g1 - g2) ** 2 + (b1 - b2) **2)
+
+def nearest_basic_color(high_color):
+
+    closest_colors = sorted(
+        BASIC_COLOR_MAP.keys(),
+        key=lambda color: rgb_distance(color, high_color)
+    )
+    return BASIC_COLOR_MAP[closest_colors[0]]
+
 
 color_const = get_const_identifiers(urwid.display_common, STYLES)
 color_const['STYLES'] = STYLES
--- urwid_utils/palette.py
+++ urwid_utils/palette.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import urwid
-from urwid_utils.colors import BASIC_COLORS, STYLES
+from urwid_utils.colors import BASIC_COLORS, STYLES, nearest_basic_color
 from urwid_utils.util import is_valid_identifier
-from urwid.display_common import _parse_color_256
+from urwid.display_common import _parse_color_256, AttrSpec
 try:
     from urwid.display_common import _parse_color_true
     URWID_HAS_TRUE_COLOR=True
 except ImportError:
     URWID_HAS_TRUE_COLOR=False
-
 
 COLORS_ALLOWED_MAP = {
     16: lambda val: val in BASIC_COLORS,
@@ -28,6 +27,12 @@ class PaletteEntry(list):
         'background_high',
     ]
 
+    DEFAULT_FG_MONO = "white"
+    DEFAULT_FG_16 = "light gray"
+    DEFAULT_BG_16 = "black"
+    DEFAULT_FG_256 = DEFAULT_FG_16
+    DEFAULT_BG_256 = DEFAULT_BG_16
+
     def __init__(self, *args, **kwargs):
         list.__init__(self, [None]*len(self.attrs))
         for index, value in enumerate(args):
@@ -35,6 +40,80 @@ class PaletteEntry(list):
             kwargs[key] = value
         for name, value in kwargs.items():
             self.__setattr__(name=name, value=value)
+
+    @classmethod
+    def from_config(
+            cls, cfg,
+            default_fg=DEFAULT_FG_16, default_bg=DEFAULT_BG_16,
+            default_fg_hi=DEFAULT_FG_256, default_bg_hi=DEFAULT_BG_256,
+            max_colors=2**24
+    ):
+        """
+        Build a palette definition from either a simple string or a dictionary,
+        filling in defaults for items not specified.
+
+        e.g.:
+            "dark green"
+                dark green foreground, black background
+
+            {lo: dark gray, hi: "#666"}
+                dark gray on 16-color terminals, #666 for 256+ color
+
+        """
+        # TODO: mono
+
+        e = PaletteEntry(mono = default_fg,
+                         foreground=default_fg,
+                         background=default_bg,
+                         foreground_high=default_fg_hi,
+                         background_high=default_bg_hi)
+
+        if isinstance(cfg, str):
+            e.foreground_high = cfg
+            if e.allowed(cfg, 16):
+                e.foreground = cfg
+            else:
+                rgb = AttrSpec(fg=cfg, bg="", colors=max_colors).get_rgb_values()[0:3]
+                e.foreground = nearest_basic_color(rgb)
+
+        elif isinstance(cfg, dict):
+
+            bg = cfg.get("bg", None)
+            if isinstance(bg, str):
+                e.background_high = bg
+                if e.allowed(bg, 16):
+                    e.background = bg
+                else:
+                    rgb = AttrSpec(fg=bg, bg="", colors=max_colors).get_rgb_values()[0:3]
+                    e.background = nearest_basic_color(rgb)
+            elif isinstance(bg, dict):
+                e.background_high = bg.get("hi", default_bg_hi)
+                if "lo" in bg:
+                    if e.allowed(bg["lo"], 16):
+                        e.background = bg["lo"]
+                    else:
+                        rgb = AttrSpec(fg=bg["lo"], bg="", colors=max_colors).get_rgb_values()[0:3]
+                        e.background = nearest_basic_color(rgb)
+
+            fg = cfg.get("fg", cfg)
+            if isinstance(fg, str):
+                e.foreground_high = fg
+                if e.allowed(fg, 16):
+                    e.foreground = fg
+                else:
+                    rgb = AttrSpec(fg=fg, bg="", colors=max_colors).get_rgb_values()[0:3]
+                    e.foreground = nearest_basic_color(rgb)
+
+            elif isinstance(fg, dict):
+                e.foreground_high = fg.get("hi", default_fg_hi)
+                if "lo" in fg:
+                    if e.allowed(fg["lo"], 16):
+                        e.foreground = fg["lo"]
+                    else:
+                        rgb = AttrSpec(fg=fg["lo"], bg="", colors=max_colors).get_rgb_values()[0:3]
+                        e.foreground = nearest_basic_color(rgb)
+        return e
+
 
     def __repr__(self):
         rep = []
